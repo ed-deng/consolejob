621d6a55b19bb61a4b7948bbb271b69417c43c74
Tommy Liang <tommyliangsays@gmail.com>
2021-03-01 16:37:49 -0500

    working on passport oauth

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const jobsRouter = require('./routes/jobs');
 
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = require('../secret.js');
+const GitHubStrategy = require('passport-github2').Strategy;
+const passport = require('passport');
+
 const app = express();
 
 const PORT = 3000;
@@ -13,6 +17,57 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 });
+
+/*******************************************************************************************************/
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: 'http://127.0.0.1:3000/auth/github/callback',
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // asynchronous verification, for effect...
+      process.nextTick(function () {
+        console.log(profile);
+        // To keep the example simple, the user's GitHub profile is returned to
+        // represent the logged-in user.  In a typical application, you would want
+        // to associate the GitHub account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+      });
+    }
+  )
+);
+
+// app.get('/account', ensureAuthenticated, function (req, res) {
+//   res.render('account', { user: req.user });
+// });
+
+app.get('/login', function (req, res) {
+  res.render('login', { user: req.user });
+});
+
+app.get(
+  '/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  function (req, res) {}
+);
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+/*******************************************************************************************************/
 
 app.use('/jobs', jobsRouter);
 
