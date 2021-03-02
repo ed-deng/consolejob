42c53b2e053f98a495b650fe0116be5ed3b62370
ed-deng <edeng2@binghamton.edu>
2021-03-02 18:56:26 -0500

    Added OAuth and fixed merge conflicts

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,11 @@
 import React, { useState, useContext, useReducer } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import JobComponent from "./components/JobComponent.jsx";
@@ -7,9 +14,9 @@ import Applied from "./components/Applied.jsx";
 import InProgress from "./components/InProgress.jsx";
 import Completed from "./components/Completed.jsx";
 import Saved from "./components/Saved.jsx";
+import LoginPage from "./components/LoginPage.jsx";
 
 export default function App() {
-  const [test, setTest] = useState(true);
   // const [testState, testDispatch] = useReducer(
   //   updateTestReducer,
   //   initialTestContext
@@ -24,66 +31,36 @@ export default function App() {
    */
 
   return (
-    <TestContext.Provider value={test}>
-      <div>
-        <Header />
-        <div style={{ display: "flex" }}>
-          <Applied />
-          <InProgress />
-          <Completed />
-          <Saved />
+    <TestContext.Provider>
+      <Router>
+        <div>
+          <li>
+            <Link to="/loginpage">Login</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
         </div>
-      </div>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/loginpage" />
+          </Route>
+          <Route path="/loginpage">
+            <LoginPage />
+          </Route>
+          <Route path="/board">
+            <div>
+              <Header />
+              <div style={{ display: "flex" }}>
+                <Applied />
+                <InProgress />
+                <Completed />
+                <Saved />
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </TestContext.Provider>
   );
 }
-
-/*
-import React, { useState, useContext, useReducer } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from 'react-router-dom';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import JobComponent from './components/JobComponent.jsx';
-import { TestContext } from './state/context';
-import LoginPage from './components/LoginPage.jsx';
-// import { initialTestContext, updateTestReducer } from "./state/reducers";
-
-export default function App() {
-  const [test, setTest] = useState(true);
-
-  <Router>
-  <div>
-    <li>
-      <Link to="/loginpage">Login</Link>
-    </li>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-  </div>
-  <Switch>
-    <Route exact path="/">
-      <Redirect to="/loginpage" />
-    </Route>
-    <Route path="/loginpage">
-      <LoginPage />
-    </Route>
-    <Route path="/board">
-      <div>
-        <h1>Job Searching</h1>
-        <button>Click me</button>
-        <p>this is dan's work</p>
-        <Header />
-        <JobComponent />
-        <Footer />
-      </div>
-    </Route>
-  </Switch>
-</Router>
-
-*/
