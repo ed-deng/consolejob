3070dfc043afc6eb2408b8e8d71b8576ebf369d9
ed-deng <edeng2@binghamton.edu>
2021-03-02 11:23:57 -0500

    New new

--- client/App.jsx
+++ client/App.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useReducer } from "react";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import JobComponent from "./components/JobComponent.jsx";
-import { TestContext } from "./state/context";
+// import { TestContext } from "./state/context";
+// import { initialTestContext, updateTestReducer } from "./state/reducers";
 
 export default function App() {
-  const [test, setTest] = useState(true);
-
-  function toggleTest() {
-    if (test === true) {
-      setTest((prevTest) => !prevTest);
-      console.log(test);
-    } else {
-      setTest(true);
-      console.log(test);
-    }
-  }
+  // const [test, setTest] = useState(true);
+  // const [testState, testDispatch] = useReducer(
+  //   updateTestReducer,
+  //   initialTestContext
+  // );
 
   return (
     <TestContext.Provider value={test}>
       <div>
         <h1>Job Searching</h1>
-        <button onClick={toggleTest}>Click me</button>
+        <button
+        // onClick={() => {
+        //   testDispatch({
+        //     type: "UPDATE_TEST",
+        //   });
+        // }}
+        >
+          Click me
+        </button>
+        <p>this is dan's work</p>
         <Header />
         <JobComponent />
         <Footer />
--- client/state/reducers.js
+++ client/state/reducers.js
@@ -10,3 +10,34 @@ export const initialCompletedState = {
 export const initialSavedState = {
   tables: [],
 };
+
+// export const initialTestContext = {
+//   boolean: false,
+// };
+
+export const userInfoState = {
+  username: "",
+  firstName: "",
+  profilePic: "",
+  email: "",
+};
+
+export const updateApplied = (state, action) => {
+  switch (action.type) {
+    case "ADD_APPLIED":
+      state.tables.push(action.payload);
+  }
+};
+
+// export const updateTestReducer = (state, action) => {
+//   switch (action.type) {
+//     case "UPDATE_TEST":
+//       return {
+//         ...state,
+//         boolean: !state.boolean,
+//       };
+//     default: {
+//       return state;
+//     }
+//   }
+// };
