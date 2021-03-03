e38248bcb65798733d9d541b7db9afda67ebfcdb
dasnyder3 <dasnyder3@gmail.com>
2021-03-03 14:12:32 -0500

    fetches user info from the backend

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,10 @@ module.exports = {
         target: "http://localhost:3000/",
         secure: false,
       },
+      "/user": {
+        target: "http://localhost:3000/",
+        secure: false,
+      },
       "/login": {
         target: "http://localhost:3000/",
         secure: false,
