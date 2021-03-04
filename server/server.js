8d5e7d0ed3ab586b3346caefbc061a87517556ed
Tommy Liang <tommyliangsays@gmail.com>
2021-03-03 20:15:26 -0500

    updated cleaning

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
-const express = require("express");
-const path = require("path");
-const jobsRouter = require("./routes/jobs");
-const db = require("./db/model");
-const session = require("express-session");
+const express = require('express');
+const path = require('path');
+const jobsRouter = require('./routes/jobs');
+const db = require('./db/model');
+const session = require('express-session');
 // const cors = require('cors');
 
-const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, SESSION_SECRET } = require("../secret.js");
-const GitHubStrategy = require("passport-github2").Strategy;
-const passport = require("passport");
+const {
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  SESSION_SECRET,
+} = require('../secret.js');
+const GitHubStrategy = require('passport-github2').Strategy;
+const passport = require('passport');
 
 const app = express();
 
@@ -23,32 +27,34 @@ passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
-app.use("/build", express.static(path.join(__dirname, "../build")));
+app.use('/build', express.static(path.join(__dirname, '../build')));
 
-app.use("/jobs", jobsRouter);
+app.use('/jobs', jobsRouter);
 
 /*******************************************************************************************************/
 
-app.use(session({ 
-  secret: SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true
- }));
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/account", function (req, res) {
-  res.render("account", { user: req.user });
+app.get('/account', function (req, res) {
+  res.render('account', { user: req.user });
 });
 
-app.get('/user', (req,res,next) => {
+app.get('/user', (req, res, next) => {
   console.log('in /user');
   if (!req.session.passport) {
-    console.log('error ocurred')
+    console.log('error ocurred');
     res.status(300).send('no user found');
   } else {
-    console.log('no error')
-    res.status(200).json(req.session.passport.user)
+    console.log('no error');
+    res.status(200).json(req.session.passport.user);
   }
 });
 
@@ -57,11 +63,11 @@ passport.use(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/auth/github/callback",
+      callbackURL: 'http://localhost:8080/auth/github/callback',
     },
     async function (accessToken, refreshToken, profile, done) {
       // asynchronous verification, for effect...
-      const findUser = "SELECT * FROM users WHERE gh_id = $1";
+      const findUser = 'SELECT * FROM users WHERE gh_id = $1';
       const params = [profile.id];
       let user = await db
         .query(findUser, params)
@@ -100,8 +106,8 @@ passport.use(
         `;
         const updateUserParams = [
           profile.id,
-          profile.profileUrl,
           profile.username,
+          profile.profileUrl,
           profile.displayName,
           profile._json.avatar_url,
         ];
@@ -117,43 +123,39 @@ passport.use(
   )
 );
 
-// app.get('/login', function (req, res) {
-//   res.render('login', { user: req.user });
-// });
-
 app.get(
-  "/auth/github",
-  passport.authenticate("github", { scope: ["user:email"] })
+  '/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] })
 );
 
 app.get(
-  "/auth/github/callback",
-  passport.authenticate("github", { failureRedirect: "/loginpage" }),
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/loginpage' }),
   function (req, res) {
     req.logIn(req.user, (err) => {
       if (err) return next(err);
-      res.redirect("/board");
-    })
+      res.redirect('/board');
+    });
   }
 );
 
-app.get("/logout", function (req, res) {
+app.get('/logout', function (req, res) {
   req.logout();
-  res.redirect("/");
+  res.redirect('/loginpage');
 });
 
 /*******************************************************************************************************/
 
-app.get("/*", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "../client/index.html"));
+app.get('/*', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 // global error handler
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: "Express error handler caught unknown middleware error",
+    log: 'Express error handler caught unknown middleware error',
     status: 400,
-    message: { err: "An error occurred" },
+    message: { err: 'An error occurred' },
   };
   const errorObj = Object.assign(defaultErr, err);
   console.log(errorObj.log);
